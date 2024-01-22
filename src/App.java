58ee267b2c9cfcb29cecdcc7baeb65e72e16e9ae
Carlos Siqueri <carlossiqueri@Carlos>
2024-01-22 13:15:44 -0300

    Exercicio resolvido

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+
+        int num = scanner.nextInt();
+
+        scanner.close();
+
+        int i = 1;
+
+        System.out.println("Tabela de multiplicação por " + num);
+
+        while (i <= 10) {
+            int result = num * i;
+            System.out.println("%s * %s = %s".formatted(num, i, result));
+            i++;
+        }
     }
 }
